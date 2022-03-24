4ee71196793e37f42289ec1911053dc2967f1333
Koifsh <95507641+Koifsh@users.noreply.github.com>
2022-03-24 11:17:10 +0000

    Added Button into a class

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import *
 import sys
 import time
 from threading import Thread
 
-class button:
-    def __init__(self, defualttitle, clicktitle, click):
-        self.butt = QPushButton(self.win)
-        self.butt.setText(defaulttitle)
-        self.butt.clicked.connect(self.clicked)
-        
-    def clicked(self):
-        self.butt.setText("Clicked")
-        def reset():
-            time.sleep(1.5)
-            self.butt.setText("Click")
-        click = Thread(target=reset, daemon=True)
-        click.start()
-
-class screen():
+class Screen:
     def __init__(self):
         app = QApplication(sys.argv)
         self.win = QMainWindow()
         self.win.setGeometry(300,300,300,300)
-        self.win.setWindowTitle("BEEEEG MUSEL")
-        self.label =QtWidgets.QLabel(self.win)
-        self.label.setText("LABEL")
+        self.win.setWindowTitle("BEEEEG MUSCLE")
 
+        buttons = []
+        for i in range(10):
+            b = Button(self,(0,i*30))
+            buttons.append(b)
 
 
         self.win.show()
         sys.exit(app.exec_())
 
-    
 
 
+class Button:
+    def __init__(self, window, xypos = (0,0), defaulttitle = "Click", clicktitle = "Clicked"):
+        self.clicktitle = clicktitle
+        self.defaulttitle = defaulttitle
+        self.butt = QPushButton(window.win)
+        self.butt.setText(defaulttitle)
+        self.butt.clicked.connect(self.clicked)
+        self.butt.move(*xypos)
+        self.butt.setStyleSheet("QPushButton::pressed""{""background-color : #808080""}")
+        
+    def clicked(self):
+        self.butt.setText(self.clicktitle)
+        def reset():
+            time.sleep(1.5)
+            self.butt.setText(self.defaulttitle)
+        Thread(target=reset, daemon=True).start()
 
-window = screen()
+window = Screen()
+
