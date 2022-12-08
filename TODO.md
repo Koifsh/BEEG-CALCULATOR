2798b41286c38fb2e0d62b89984f99fc411c68b7
Koifsh <95507641+Koifsh@users.noreply.github.com>
2022-12-08 18:05:59 +0000

    Update to a user screen

--- TODO.md
+++ TODO.md
@@ -1,8 +1,10 @@
 TODO:
-- UI
+- User implementation
+- Workout tracker
+
 - ! Data logging (previous bmi ,height , weight etc...)
 - one-repmax calcualtor
 - maxiumum heart rate
 - daily calory calcualtor (maintainance cals, calory counter)
 - body mass index calculation
-- more
+- Security
--- main.py
+++ main.py
@@ -1,34 +1,66 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-import sys
-import time
-from threading import Thread
-import pandas
+import sys,pandas,time
 import matplotlib.pyplot as plt
-
-
+from threading import Thread
 
 class Screen(QMainWindow):
-    def __init__(self):
+    def init(self):
         super(Screen,self).__init__()
         self.setGeometry(300,300,600,600)
         self.setWindowTitle("BEEEEGGG calculator")
         self.setStyleSheet("background: #161219;")
         try :
             self.data = pandas.read_csv("data.csv")
+            self.startscreen()
         except FileNotFoundError:
-            frame = dict(weight=[0], height=[0], hours=[0])
-            #creates a dataframe with the datatypes of the columns defined
+            frame = dict(username=[],password=[])
             self.data = pandas.DataFrame(frame)
-        self.mainscreen()
-        self.show()
+            self.data.to_csv("data.csv",mode="w",index=False)
+            self.startscreen()
+            
         
+    def startscreen(self):
+        self.widgets = {
+            "title": Text("Beeg Calculator",(225,10),15),
+            "login": Button("Login",(200,60),func="loginscreen"),
+            "createuser" : Button("Create new user",(200,140),func="createuserscreen")
+        }
+        self.update()
+    
     def mainscreen(self):
         self.widgets = {
-            "title": Text(self,"Beeg Calculator",(225,10),15),
-            "data": Button(self,"Show data",(200,60))
+            "title": Text("Beeg Calculator",(225,10),15),
+            "data": Button("Show Data",(200,60),func="datascreen")
         }
+        self.update()
+    
+    def createuserscreen(self):
+        self.widgets = {
+            "back" : Button("Back",(10,10),(100,50),func="startscreen"),
+            "title": Text("Create New User",(225,10),15),
+            "username": LineEdit("Username",(200,60)),
+            "password": LineEdit("Password",(200,120)),
+            "submit": Button("Submit",(200,180),func="submitcreateuser")
+        }
+        self.widgets["password"].setEchoMode(QLineEdit.Password)
+        self.update()
+        
+        
+    def datatable(self):
+        self.datatable = DataFrameEditor(self)
+        self.datatable.show()
+    
+    def loginscreen(self):
+        self.widgets = {
+            "back" : Button("Back",(10,10),(100,50),func="startscreen"),
+            "title": Text("Login",(225,10),15),
+            "username": LineEdit("Username",(200,60)),
+            "password": LineEdit("Password",(200,120)),
+            "submit": Button("Submit",(200,180),func="submitlogin")
+        }
+        self.widgets["password"].setEchoMode(QLineEdit.Password)
         self.update()
     
     def datascreen(self):
@@ -36,36 +68,32 @@ class Screen(QMainWindow):
         previousheight = self.data["height"].iloc[-1]
         previoushours = self.data["hours"].iloc[-1]
         self.widgets = {
-            "back" : Button(self,"Back",(10,10),(100,50)),
-            "title": Text(self,"Data",(225,10),15),
-            "weightbox" : LineEdit(self,"Weight",(200,70)),
-            "heightbox" : LineEdit(self,"Height",(200,130)),
-            "hoursbox" : LineEdit(self,"Hours",(200,190)),
-            "submit" : Button(self,"Submit",(200,250),(200,50)),
-            "weights" : Text(self,f"Previous weight = {previousweight} ",(10,300),15),
-            "heights" : Text(self,f"Previous height = {previousheight}",(10,320),15),
-            "hours" : Text(self,f"Previous hours = {previoushours} ",(10,340),15),
-            "fulldata" : Button(self,"Show Full Data",(470,10),(120,50))
+            "back" : Button("Back",(10,10),(100,50),func="startscreen"),
+            "title": Text("Data",(225,10),15),
+            "weightbox" : LineEdit("Weight",(200,70)),
+            "heightbox" : LineEdit("Height",(200,130)),
+            "hoursbox" : LineEdit("Hours",(200,190)),
+            "submit" : Button("Submit",(200,250),(200,50),func="saveresults"),
+            "weights" : Text(f"Previous weight = {previousweight} ",(10,300),15),
+            "heights" : Text(f"Previous height = {previousheight}",(10,320),15),
+            "hours" : Text(f"Previous hours = {previoushours} ",(10,340),15),
+            "fulldata" : Button("Show Full Data",(470,10),(120,50),func="tablescreen")
         }
         self.update()
         
-        
     def update(self):
-        for widget in self.widgets.values():
+        for key, widget in self.widgets.items():
+            print(key)
             widget.show()
-        
-
+        print()
     def clearscreen(self):
-        tempwidgets = dict(self.widgets)
-        for key,value in self.widgets.items():
-            value.deleteLater()
-            del tempwidgets[key]
-        self.widgets = tempwidgets
-        print(self.widgets)
-    
-    def closeEvent(self, event) :
-        self.data.to_csv("data.csv",mode="w",index=False)
-        event.accept()
+        print("screen cleared")
+        for value in self.widgets.values():
+            value.setParent(None)
+
+
+app = QApplication(sys.argv)
+window = Screen()
 
 class DataFrameEditor(QWidget):
     def __init__(self):
@@ -78,14 +106,9 @@ class DataFrameEditor(QWidget):
         self.table = Table(self.data)
         self.setLayout(mainLayout)
         mainLayout.addWidget(self.table)
-        buttonexport = Button(self,"Save edited changes")
-        mainLayout.addWidget(buttonexport)
-        buttonrevert = Button(self,"Revert changes")
-        mainLayout.addWidget(buttonrevert)
-        
-
+            
 class Table(QTableWidget):
-    def __init__(self, data):
+    def __init__(self,data):
         self.tabledata = data
         super().__init__()
         rowcount, columncount = self.tabledata.shape
@@ -137,7 +160,7 @@ class Table(QTableWidget):
             self.item(row,column).setText(str(window.data.iloc[row,column]))
                 
 class LineEdit(QLineEdit):
-    def __init__(self,window,text,pos,size=(200,50)):
+    def __init__(self,text,pos,size=(200,50)):
         super().__init__(window)
         self.move(*pos)
         self.setPlaceholderText(text)
@@ -155,7 +178,7 @@ class LineEdit(QLineEdit):
         )
 
 class Text(QLabel):
-    def __init__(self,window, text,pos,size):
+    def __init__(self,text,pos,size):
         super().__init__(text,window)
         self.move(*pos)
         self.setAlignment(Qt.AlignVCenter)
@@ -170,11 +193,10 @@ class Text(QLabel):
         self.setFixedSize(size*len(text),size*3)    
 
 class Button(QPushButton):
-    def __init__(self,window, text,pos=None,size = (200,70)):
-        super().__init__(text,window)
-        self.win = window
+    def __init__(self,text,pos=None,size = (200,70),func="notentered"):
+        super().__init__(text, window)
         self.cooldownstate = False
-        self.message = text
+        self.func = func
         if pos is not None:
             self.move(*pos)
         if size is not None:
@@ -196,18 +218,18 @@ class Button(QPushButton):
         }
         ''')
         self.clicked.connect(self.buttfunctions)
-    
-    
+
+
     def buttfunctions(self):
         if not self.cooldownstate:
-            match self.message:
-                case "Show data":
+            match self.func:
+                case "datascreen":
                     window.clearscreen()
                     window.datascreen()
-                case "Back":
+                case "startscreen":
                     window.clearscreen()
-                    window.mainscreen()
-                case "Submit":
+                    window.startscreen()
+                case "saveresults":
                     editboxes = [window.widgets[i].text() for i in ["weightbox","heightbox","hoursbox"]]
                     if all((i.isnumeric() or isfloat(i)) for i in editboxes):
                         newrow = pandas.DataFrame.from_records([{"weight" : float(editboxes[0]),
@@ -219,26 +241,56 @@ class Button(QPushButton):
                         self.notice(0.5,"Submitted successfully","Submit")
                     else:
                         self.notice(0.5,"Values entered incorrectly","Submit")
-                
-                case "Show Full Data":
-                    window.datatable = DataFrameEditor()
-                    window.datatable.show()
-                
-                case "Save edited changes":
-                    window.data = window.datatable.data
-                    window.datascreen()
-                
-                case "Revert changes":
-                    window.datatable.__init__()
-                    
+                case "tablescreen":
+                    window.datatable()           
+                case "submitlogin":
+                    editboxes = [window.widgets[i].text() for i in ["username","password"]]
+                    if all(i == "" for i in editboxes):
+                        self.notice(0.5,"Boxes aren't filled","Submit")
+                    else:
+                        if editboxes[0] not in set(window.data["username"]):
+                            self.notice(0.5,"Username does not exist","Submit")
+                            print(window.data["username"])
+                        else:
+                            print(window.data.loc[window.data["username"] == editboxes[0],"password"].values[0])
+                            if window.data.loc[window.data["username"] == editboxes[0],"password"].values[0] != editboxes[1]:
+                                self.notice(0.5,"Password incorrect","Submit")
+                            else:
+                                #placeholder
+                                window.clearscreen()
+                                window.mainscreen()
+                                
+                case "loginscreen":
+                    window.clearscreen()
+                    window.loginscreen()
+                case "notentered":
+                    print("Button function must be entered")
+                case "submitcreateuser":
+                    editboxes = [window.widgets[i].text() for i in ["username","password"]]
+                    if all(i == "" for i in editboxes):
+                        self.notice(0.5,"Boxes aren't filled","Submit")
+                    else:
+                        if editboxes[0] in set(window.data):
+                            self.notice(0.5,"Username already exists","Submit")
+                        else:
+                            newrow = pandas.DataFrame.from_records([{"username":editboxes[0],"password":editboxes[1]}])
+                            window.data = pandas.concat([window.data, newrow])
+                            window.data.reset_index(drop=True)
+                            self.notice(0.5,"User created","Submit")
+                            window.data.to_csv("data.csv",mode="w",index=False)
+                case "createuserscreen":
+                    window.clearscreen()
+                    window.createuserscreen()
+
     def notice(self, sleeptime, message, orgmessage):
-        def noticethread():
+        def noticefunc():
             self.cooldownstate = True
             self.setText(message)
             time.sleep(sleeptime)
             self.setText(orgmessage)
             self.cooldownstate = False
-        Thread(target=noticethread, daemon = True).start()
+        self.noticethread = Thread(target=noticefunc, daemon = True)
+        self.noticethread.start()
 
 def isfloat(num):
     try:
@@ -246,8 +298,8 @@ def isfloat(num):
         return True
     except ValueError:
         return False
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = Screen()
+    window.init()
     window.show()
     sys.exit(app.exec_())
